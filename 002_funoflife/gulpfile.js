2b73af38b9d57192bf703facc09c6ee2944c7042
yuji tsuburaya <nikke.934@gmail.com>
2017-09-18 12:51:56 +0900

    :+1: 求人詳細ページ作成

--- 002_funoflife/gulpfile.js
+++ 002_funoflife/gulpfile.js
@@ -2,6 +2,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+      .pipe(webserver({
+        fallback:   'index.html',
+        livereload: true,
+        directoryListing: {
+          enable: true,
+          path: './'
+        },
+        open: true
+
+      }));
+});
 
 gulp.task('sass', function() {
     gulp.src('./css/*.scss')
@@ -12,5 +27,5 @@ gulp.task('sass', function() {
 });
 
 gulp.task('default', function() {
-    gulp.watch('./css/*.scss', ['sass']);
+  gulp.watch('./css/*.scss', ['sass']);
 });
